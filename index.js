263f92668c901f544798276d86cf50139fc6a556
dnjsskarb7 <64752079+dnjsskarb7@users.noreply.github.com>
2020-09-11 02:00:20 +0900

    a

--- index.js
+++ index.js
@@ -38,13 +38,6 @@ app.use("/api/v1", api);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Listening on ${process.env.PORT}...👀`);
 });
